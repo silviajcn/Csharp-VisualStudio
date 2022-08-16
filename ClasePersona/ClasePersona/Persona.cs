1e90cf8357ba9c471d65c517f7f0a2bc8dd10d19
Silvia <silviajcn@gmail.com>
2022-08-16 14:45:34 -0500

    changes

---
+++ ClasePersona/ClasePersona/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePersona
+{
+    internal class Persona
+    {
+        public string Nombre { get; set;}
+        public string Apellido { get; set; }
+
+        private int _edad;
+        public int Edad {
+            get {
+                return _edad;
+            }
+            set { 
+                _edad = value >= 0 ? value : 0;
+            }
+        }
+
+        //Constructores:
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public Persona(string nombre, string apellido) : this(nombre)
+        {
+            Apellido = apellido;
+        }
+
+        public Persona(string nombre, string apellido, int edad) : this(nombre, apellido)
+        {
+            Edad = edad;
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola, mi nombre es {0} {1}", Nombre, Apellido);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Nombre: {0}, Apellido: {1}, Edad: {2}", Nombre, Apellido, Edad);
+        }
+
+    }
+}
--- ClasePersona/ClasePersona/Program.cs
+++ ClasePersona/ClasePersona/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClasePersona;
+
+Persona persona1 = new Persona("Juan", "Perez", 28);
+Persona persona2 = new Persona("Luis", "Pereira", 26);
+
+persona1.Saludar();
+persona2.Saludar();
+
+//Comprobar la validacion de la edad:
+persona1.Edad = -10;
+Console.WriteLine(persona1);
