e0e1a435550a6f06c6d7b1ac7cd295bcff2609e3
Silvia <silviajcn@gmail.com>
2022-08-17 17:19:33 -0500

    arrays

---
+++ UsoDeArreglos/UsoDeArreglos/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Ingresa el tamaño del arreglo:");
+int tan = Convert.ToInt32(Console.ReadLine());
+
+// Definir arreglo:
+int[] numeros = new int[tan];
+
+//leer datos y guardarlos en el arreglo:
+for(int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Ingresa el numero en el indice {0}", i);
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+//Mostrar datos:
+Console.WriteLine("Los datos que ingresaste son:");
+MostrarArreglo(numeros);
+
+//metodo para imprimir arreglos:
+static void MostrarArreglo(int[] arreglo)
+{
+    foreach(int num in arreglo)
+    {
+        Console.Write("{0} ", num);
+    }
+    Console.WriteLine();
+}
